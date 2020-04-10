3ea66ab4c60d55a34b1b98142d15f1fcdd197b11
Julio Vasconez Yulan <jvasconez28@gmail.com>
2020-04-10 21:41:22 +0200

    Updates

---
+++ precache-manifest.f82a0834e6ad54c5166c489d12164d2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af4d60ae2652047795a037aa9e8533a",
+    "url": "/dhtml/index.html"
+  },
+  {
+    "revision": "a69044a1d14bf96e69f8",
+    "url": "/dhtml/static/css/main.336e5315.chunk.css"
+  },
+  {
+    "revision": "b253528abab28624e67d",
+    "url": "/dhtml/static/js/2.ad178ca8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dhtml/static/js/2.ad178ca8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a69044a1d14bf96e69f8",
+    "url": "/dhtml/static/js/main.f80805e1.chunk.js"
+  },
+  {
+    "revision": "bb8d384b9d0f425b1a2a",
+    "url": "/dhtml/static/js/runtime-main.fc5cbe14.js"
+  }
+]);
